ff33a7f25c5eed8c7885a788fe6daa46acc658db
tamia gladney <tamia324@live.unc.edu>
2021-02-08 20:27:01 -0500

    Progress on exercise 1

--- exercises/ex01/fortune_cookie.py
+++ exercises/ex01/fortune_cookie.py
@@ -1,8 +1,8 @@
 """Program that outputs one of at least four random, good fortunes."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
+__author__ = "730337008"
 
-# The randint function is imported from the random library so that
+# andint function is imported from the random library so that
 # you are able to generate integers at random.
 # 
 # Documentation: https://docs.python.org/3/library/random.html#random.randint
@@ -13,3 +13,16 @@ from random import randint
 
 
 # Begin your solution here...
+print("your fortune cookie says...")
+fun: int=randint(1,4)
+if fun == 1:
+   print("Your future is bright.")
+else: 
+   if fun == 2:
+    print("You have good things in your future.")
+   else:
+      if fun == 3:
+       print("Maybe you will buy a dog in the near future")
+      else:
+         print("You will have a great dinner")
+print("Now, go spread positive vibes!")
--- exercises/ex01/tar_heels.py
+++ exercises/ex01/tar_heels.py
@@ -1,6 +1,19 @@
 """An exercise in remainders and boolean logic."""
 
-__author__ = "ENTER YOUR 9-DIGIT PID HERE"
-
+__author__ = 730337008
 
 # Begin your solution here...
+integer: int = int(input("Pick an integer"))
+
+if (integer % 7==0 and integer % 2==0):
+    print("TAR HEELS") 
+elif (integer % 2==0):
+    print("TAR") 
+elif (integer % 7==0):
+    print("HEELS")
+
+else: 
+    print("CAROLINA")
+
+
+
